3067e89da707cd68a1c29abb132464da35338baf
Yussif <djiceman.gh@gmail.com>
2021-03-13 22:06:03 +0000

    attachment edit

--- attachment.php
+++ attachment.php
@@ -2,32 +2,34 @@
 
 <div id="primary" class="content-area">
 
-    <main id="main" class="site-main" role="main">
-        <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
+  <main id="main" class="site-main" role="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
 
-          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-              <header class="entry-header">
-                  <?php the_title('<h1>', '</h1>'); ?>
-              </header>
+          <header class="entry-header">
+            <?php the_title('<h1>', '</h1>'); ?>
+          </header>
 
-              <div class="entry-content">
-                <pre><?php var_export($post); ?></pre>
+          <div class="entry-content">
+            
 
-                  <?php the_content(); ?>
-              </div>
-              <?php if (comments_open()) : ?>
-              <?php comments_template(); ?>
-            <?php endif; ?>
-          </article>
+            <p><a href="<?php echo $post->guid; ?>">Download</a></p>
+
+            <?php the_content(); ?>
+          </div>
+          <?php if (comments_open()) : ?>
+            <?php comments_template(); ?>
+          <?php endif; ?>
+        </article>
 
 
-            <?php endwhile;
-        else : ?>
-            <?php get_template_part('template-parts/content', 'none'); ?>
-        <?php endif; ?>
-    </main>
-            <p>ATTACHMENT</p>
+      <?php endwhile;
+    else : ?>
+      <?php get_template_part('template-parts/content', 'none'); ?>
+    <?php endif; ?>
+  </main>
+  <p>ATTACHMENT</p>
 </div>
 
 <?php get_sidebar(); ?>
